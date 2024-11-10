17baf9f070417bc02f3a105e171338e589e4370c
Faize <faize@gmail.com>
2024-11-10 15:13:13 +0530

    Products Listing Added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,7 @@ import AdminRoute from './components/Auth/AdminRoute.jsx'
 import AddProduct from './admin/AddProduct.jsx'
 import Order from './admin/Order.jsx'
 import Transactions from './admin/Transactions.jsx'
+import ProductList from './admin/ProductList.jsx'
 
 
 
@@ -106,6 +107,14 @@ const router = createBrowserRouter([
         element: (
           <AdminRoute>
             <Transactions />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/admin/product-list",
+        element: (
+          <AdminRoute>
+            <ProductList />
           </AdminRoute>
         ),
       },
