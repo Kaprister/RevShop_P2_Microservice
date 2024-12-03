dd7b7468457490d34305121ffd04eb8149ad5c51
Kaprister <sushantkapri@gamil.com>
2024-12-03 17:03:30 +0530

    authentication added

---
+++ api-gateway/src/main/java/com/revature/apigateway/util/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.revature.apigateway.util;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+
+    public static final String SECRET = "6LeY2hf3JHDX6IyAm60cRGWhvJx+LjUQ9QVRH6A6Giw=";
+
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
