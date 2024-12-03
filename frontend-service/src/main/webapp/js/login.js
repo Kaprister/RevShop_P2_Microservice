2968e9ac2e518266955251e616a20b7a42a5bbef
Faize <faize@gmail.com>
2024-12-03 17:07:00 +0530

    Update

--- frontend-service/src/main/webapp/js/login.js
+++ frontend-service/src/main/webapp/js/login.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		const password = document.getElementById("password").value;
 
         try {
-            const response = await fetch("http://localhost:8087/auth/login", {
+            const response = await fetch("http://localhost:8081/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, password }),
